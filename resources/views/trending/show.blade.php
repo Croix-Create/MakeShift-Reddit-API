8de2043988b4b366eb2ebf8c7ed42ee442ba1d98
Croix-Create <croix28@gmail.com>
2024-07-03 21:59:11 +0200

    Patched UI bug on home page - Reddit web-scrape page // Added clickable links to relevent posts

--- resources/views/trending/show.blade.php
+++ resources/views/trending/show.blade.php
@@ -4,20 +4,8 @@
 
         <main class="max-w-6xl mx-auto mt-10 lg:mt-20 space-y-6">
             <article class="max-w-4xl mx-auto lg:grid lg:grid-cols-12 gap-x-10">
-                <div class="col-span-4 lg:text-center lg:pt-14 mb-10">
                 @foreach ($posts as $post)
 
-                    <div class="flex items-center lg:justify-center text-sm mt-4">
-                        <img src="/images/lary-avatar.svg">
-                        <div class="ml-3 text-left">
-                            <h5 class="font-bold">
-                            <div>
-                                By {!! $post->author !!}
-                            </div>
-                            </h5>
-                        </div>
-                    </div>
-                </div>
 
                 <div class="col-span-8">
                     <div class="hidden lg:flex justify-between mb-6">
@@ -37,20 +25,19 @@
                         </a>
                     </div>
 
-                    <ul>
-                        
-                            <li>
-                            <h1 class="font-bold text-3xl lg:text-4xl mb-10 text-blue-600">
-                                {!! $post->title !!}
-                            </h1>
+                    <ul> 
+                        <li>
+                        <h1 class="font-bold text-3xl lg:text-4xl mb-10 text-blue-600">
+                            {!! $post->title !!}
+                        </h1>
 
-                            <div>
-                                <img src="{!! $post->url !!}">
-                            </div>
-                            <div>
-                                {!! $post->score !!}
-                            </div>
-                            </li>
+                        <div>
+                            <a href="{!! $post->url !!}"><img src="{!! $post->url !!}"></a>
+                        </div>
+                        <div>
+                            {!! $post->score !!}
+                        </div>
+                        </li>
                         @endforeach
                     </ul>
 
